41eebf9d454f3baee4f66533c7aad8d012f1f4f8
roulfe <roulfej@gmail.com>
2026-04-21 17:05:36 -0500

    team page animation

--- blendart-barber/src/components/Team.jsx
+++ blendart-barber/src/components/Team.jsx
@@ -1,15 +1,109 @@
 import TeamCard from "./TeamCard.jsx";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function Team() {
+  const container = useRef(null);
+
+  useGSAP(() => {
+  const copyItems = container.current.querySelectorAll(".gsap-copy");
+  const teamCards = container.current.querySelectorAll(".gsap-team-card");
+  
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: container.current,
+      start: "top 85%",
+      toggleActions: "play none none none",
+      once: true,
+    },
+  });
+
+  if (copyItems.length) {
+    tl.fromTo(
+      copyItems,
+      {
+        y: 30,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power2.out",
+        stagger: 0.15,
+      }
+    );
+  }
+
+  if (teamCards.length) {
+    tl.fromTo(
+      teamCards,
+      {
+        y: 30,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power2.out",
+        stagger: 0.15,
+      },
+      "-=0.5"
+    );
+  }
+}, { scope: container });
+
   return (
-    <section className="min-h-[calc(100vh-96px)] items-center py-10 px-4 bg-(--Egg)">
-      <h2 className="pb-10 my-10 text-center text-3xl font-bold md:text-4xl">Our Team</h2>
+    <section
+      ref={container}
+      className="min-h-[calc(100vh-96px)] px-4 py-10 bg-(--Egg)"
+    >
+      <h2 className="gsap-copy my-10 pb-10 text-center text-3xl font-bold md:text-4xl">
+        Our Team
+      </h2>
+
       <div className="flex flex-wrap justify-center gap-10">
-        <TeamCard image="/src/assets/photos/Marlowe.jpg" name="Marlowe" alt="Profile picture of Marlowe" link="https://www.fresha.com/a/blendart-barber-studio-frankfort-20867-south-la-grange-road-kf4e35li/booking?menu=true&dppub=true&cartId=90fa0ba8-e072-449b-a31f-11d2f3eb276c" />
-        <TeamCard image="/src/assets/photos/Dom2.jpg" name="Dominic" alt="Profile picture of Dominic" link="https://www.fresha.com/a/blendart-barber-studio-frankfort-20867-south-la-grange-road-kf4e35li/booking?menu=true&dppub=true&cartId=90fa0ba8-e072-449b-a31f-11d2f3eb276c" />
-        <TeamCard image="/src/assets/photos/Mo.jpg" name="Mo" alt="Profile picture of Mo" link="https://www.fresha.com/a/blendart-barber-studio-frankfort-20867-south-la-grange-road-kf4e35li/booking?menu=true&dppub=true&cartId=90fa0ba8-e072-449b-a31f-11d2f3eb276c" />
-        <TeamCard image="/src/assets/photos/Ashley.jpg" name="Ashley" alt="Profile picture of Ashley" link="https://app.thecut.co/barbers/barberashley" />
-        <TeamCard image="/src/assets/photos/Christina3.png" name="Christina" alt="Profile picture of Christina" link="https://booksy.com/en-us/709062_lady-barber-smalls_barber-shop_19304_frankfort?do=invite&_branch_match_id=1544832368508364856&utm_medium=profile_share_from_profile&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXT07J0UvKz88urtRLzs%2FVzzdx9zI2y8mKjEiyrytKTUstKsrMS49PKsovL04tsnXOKMrPTQUASvLicTwAAAA%3D" />
+        <TeamCard
+          className="gsap-team-card"
+          image="/src/assets/photos/Marlowe.jpg"
+          name="Marlowe"
+          alt="Profile picture of Marlowe"
+          link="https://www.fresha.com/a/blendart-barber-studio-frankfort-20867-south-la-grange-road-kf4e35li/booking?menu=true&dppub=true&cartId=90fa0ba8-e072-449b-a31f-11d2f3eb276c"
+        />
+        <TeamCard
+          className="gsap-team-card"
+          image="/src/assets/photos/Dom2.jpg"
+          name="Dominic"
+          alt="Profile picture of Dominic"
+          link="https://www.fresha.com/a/blendart-barber-studio-frankfort-20867-south-la-grange-road-kf4e35li/booking?menu=true&dppub=true&cartId=90fa0ba8-e072-449b-a31f-11d2f3eb276c"
+        />
+        <TeamCard
+          className="gsap-team-card"
+          image="/src/assets/photos/Mo.jpg"
+          name="Mo"
+          alt="Profile picture of Mo"
+          link="https://www.fresha.com/a/blendart-barber-studio-frankfort-20867-south-la-grange-road-kf4e35li/booking?menu=true&dppub=true&cartId=90fa0ba8-e072-449b-a31f-11d2f3eb276c"
+        />
+        <TeamCard
+          className="gsap-team-card"
+          image="/src/assets/photos/Ashley.jpg"
+          name="Ashley"
+          alt="Profile picture of Ashley"
+          link="https://app.thecut.co/barbers/barberashley"
+        />
+        <TeamCard
+          className="gsap-team-card"
+          image="/src/assets/photos/Christina3.png"
+          name="Christina"
+          alt="Profile picture of Christina"
+          link="https://booksy.com/en-us/709062_lady-barber-smalls_barber-shop_19304_frankfort?do=invite&_branch_match_id=1544832368508364856&utm_medium=profile_share_from_profile&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXT07J0UvKz88urtRLzs%2FVzzdx9zI2y8mKjEiyrytKTUstKsrMS49PKsovL04tsnXOKMrPTQUASvLicTwAAAA%3D"
+        />
       </div>
     </section>
   );
--- blendart-barber/src/components/TeamCard.jsx
+++ blendart-barber/src/components/TeamCard.jsx
@@ -3,7 +3,7 @@
 function TeamCard(props) {
 
     return(
-            <div className="hover:scale-105 duration-300 cursor-pointer relative group">
+            <div className={`opacity-0 hover:scale-105 duration-300 cursor-pointer relative group ${props.className || ""}`}>
                 <img src={props.image} alt={props.alt} className="w-64 h-96 object-cover object-top rounded-t-3xl " />
                 <p className="w-64 text-center bg-black text-white py-6 rounded-b-3xl">{props.name}</p>
                     <a
