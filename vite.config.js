aeafb9ed255f13e48d011511b1ca3b4af1e3cb98
roulfe <roulfej@gmail.com>
2026-04-22 16:46:21 -0500

    fixed root

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/blendart-barber-studio/',
   plugins: [react(), tailwindcss()],
 })
